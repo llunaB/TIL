189aebb584f1855ee4f2eb361e181d164bf10b27
ejbang <euijin.bang@gmail.com>
2021-08-28 00:25:22 +0900

    algo study update

--- algo_study/telecom/s1.py
+++ algo_study/telecom/s1.py
@@ -11,47 +11,47 @@ for tc in range(1, T+1):
 
     di = [0, 1, 0, -1]
     dj = [1, 0, -1, 0]
-    k = 0
+
 
     pass_list = ['X', 'A', 'B', 'C', 'DD']
 
     for i in range(N):
         for j in range(N):
+            k = 0
             if arr[i][j] == 'A':
-                # 네방향 돈다 + 'X' 와 'B', 'C', ' ' 는 그냥 두고 'H'이면 'H'를 공백으로 치환
+                # 네방향 돈다 + 'X' 와 'A', 'B', 'C', 'DD' 는 그냥 두고 'H'이면 'DD'로 치환
                 while k < 4:
-                        ni, nj = i+di[k], j+dj[k]
-                        if 0 <= ni < N and 0 <= nj < N:
-                            if arr[ni][nj] in pass_list:
-                                pass
-                            elif arr[ni][nj] == 'H':
-                                arr[ni][nj] = arr[ni][nj].replace('H','DD')
-                            k = k+1
+                    ni, nj = i+di[k], j+dj[k]
+                    if 0 <= ni < N and 0 <= nj < N:
+                        if arr[ni][nj] in pass_list:
+                            pass
+                        elif arr[ni][nj] == 'H':
+                            arr[ni][nj] = 'DD'
+                    k = k+1
 
             if arr[i][j] == 'B':
-                # 네 방향을 두 번 돈다 + 'X' 와 'B', 'C', ' ' 는 그냥 두고 'H'이면 'H'를 공백으로 치환
+                # 네 방향을 두 번씩 돈다 + 'X' 와 'A', 'B', 'C', 'DD' 는 그냥 두고 'H'이면 'DD'로 치환
                 while k < 4:
-                    for t in range(2):
-                        ni, nj = i + di[k], j + dj[k]
-                        if 0 <= ni < N and 0 <= nj < N:
-                                if arr[ni][nj] in pass_list:
-                                    pass
-                                elif arr[ni][nj] == 'H':
-                                    arr[ni][nj].replace('H','DD')
-                                k = k+1
-
-            if arr[i][j] == 'C':
-                # 네 방향을 세 번 돈다 + 'X' 와 'B', 'C' , ' ' 는 그냥 두고 'H'이면 'H'를 공백으로 치환
-                while k < 4:
-                    for t in range(3):
-                        ni, nj = i+di[k], j+dj[k]
+                    for t in range(1, 3):
+                        ni, nj = i + di[k]*t, j + dj[k]*t
                         if 0 <= ni < N and 0 <= nj < N:
                             if arr[ni][nj] in pass_list:
                                 pass
                             elif arr[ni][nj] == 'H':
-                                arr[ni][nj].replace('H','DD')
-                            k = k+1
+                                arr[ni][nj] = 'DD'
+                    k = k+1
 
+            if arr[i][j] == 'C':
+                # 네 방향을 세 번씩 돈다 + 'X' 와 'A', 'B', 'C' , 'DD' 는 그냥 두고 'H'이면 'DD'로 치환
+                while k < 4:
+                    for t in range(1, 4):
+                        ni, nj = i + di[k]*t, j + dj[k]*t
+                        if 0 <= ni < N and 0 <= nj < N:
+                            if arr[ni][nj] in pass_list:
+                                pass
+                            elif arr[ni][nj] == 'H':
+                                arr[ni][nj] = 'DD'
+                    k = k+1
 
     cnt = 0
     for i in range(N):
@@ -59,4 +59,4 @@ for tc in range(1, T+1):
             if arr[i][j] == 'H':
                 cnt += 1
 
-    print(cnt)
+    print("#{} {}".format(tc, cnt))
